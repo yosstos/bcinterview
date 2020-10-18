f5f0b7146adde2522099b61b3eded5aa7753d9e2
Yossi Bachar <yossi.bachar@mail.huji.ac.il>
2020-10-19 01:48:59 +0300

    Updates

---
+++ precache-manifest.65cfadb6f688d99483d99a232eaa5d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5dacfd89937eac96ddb711c2bfac3f7",
+    "url": "/bcinterview/index.html"
+  },
+  {
+    "revision": "577aa6a3c8fe2701c853",
+    "url": "/bcinterview/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe8224c365976e89c76f",
+    "url": "/bcinterview/static/js/2.275c8d84.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/bcinterview/static/js/2.275c8d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577aa6a3c8fe2701c853",
+    "url": "/bcinterview/static/js/main.32a40a8d.chunk.js"
+  },
+  {
+    "revision": "983d86d93555296e1525",
+    "url": "/bcinterview/static/js/runtime-main.71642ac2.js"
+  }
+]);
